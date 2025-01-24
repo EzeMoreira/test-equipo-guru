79f8b586d2ef6b1b69f02126fd6e106747436fea
EzequielMoreira <eezequielmoreira@gmail.com>
2025-01-24 12:39:29 -0300

    ultimos cambios tanto en estilo como codigo js

--- codigo.js
+++ codigo.js
@@ -1,14 +1,15 @@
-// Seleccionar el formulario
+const boton = document.getElementById('suscrito');
+
+function mostrarCartel(){
+    boton.innerHTML = "Suscrito con exito"
+}
+
 const form = document.getElementById("subscription-form");
 
-// Manejar el envío del formulario
 form.addEventListener("submit", (event) => {
-  event.preventDefault(); // Evitar recargar la página
+  event.preventDefault();
 
-  // Obtener el valor del email
   const email = document.getElementById("email").value;
 
-  // Mostrar un mensaje en la consola (o realizar una acción con el correo)
   console.log(`Correo ingresado: ${email}`);
-  alert("¡Gracias por suscribirte!");
 });
